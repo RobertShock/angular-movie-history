da20327b05e50b8a546b5f3f47fc3232635f24a2
Robbie Shock <rob.f.shock@gmail.com>
2017-11-14 20:14:06 -0600

    xxx

--- javascripts/controllers/SearchCtrl.js
+++ javascripts/controllers/SearchCtrl.js
@@ -1,22 +1,15 @@
 'use strict';
 
 app.controller("SearchCtrl", function($scope, tmdbService){
-  
-  $scope.enterPush = (event) =>{
-    if(event.keyCode === 13){
-      tmdbService.searchMovies(event.target.value).then((results) =>{
-        console.log("movies?", results.data.results);
-      }).catch((err) =>{
-        console.log("error in searchMovies", err);
-      });
-    }
-  };
-  
-  
-  
+	$scope.movies = [];
 
-
-
-
-
+	 $scope.enterPush = (event) =>{
+	 	if(event.keyCode === 13){
+	 		tmdbService.searchMovies(event.target.value).then((results) =>{
+				$scope.movies = results.data.results; 
+			}).catch((err) =>{
+				console.log("error in searchMovies", err);
+			});
+	 	}
+	};	
 });
