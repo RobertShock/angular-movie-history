8ae8f5e776da6794d69fa537c203c8d80f3fac30
Robbie Shock <rob.f.shock@gmail.com>
2017-11-15 21:47:23 -0600

    completed delete wishlist option

--- javascripts/controllers/WishlistCtrl.js
+++ javascripts/controllers/WishlistCtrl.js
@@ -3,9 +3,25 @@
 app.controller("WishlistCtrl", function($rootScope, $scope, MovieService){
 	$scope.movies = [];
 
+	const getMovies = () => {
+		MovieService.getWishlistMovies($rootScope.uid).then((results) =>{
+			$scope.movies = results;
+		}).catch((err) =>{
+			console.log("error in getWishlistMovies", err);
+		});
+	};
+	
 	MovieService.getWishlistMovies($rootScope.uid).then((results) =>{
 		$scope.movies = results;
 	}).catch((err) =>{
 		console.log("error in getWishlistMovies", err);
 	});
+	
+	$scope.deleteMovie = (movieId) => {
+		MovieService.deleteMovie(movieId).then((result) => {
+			getMovies();
+		}).catch((err) =>{
+			console.log("error in deleteMovie", err);
+		});
+	};
 });
